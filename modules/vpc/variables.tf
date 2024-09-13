7bcb14d9c0302f4712a3d86250ee1648090dd2cb
Nataliia Vorobets <157847383+NatVor@users.noreply.github.com>
2024-09-13 09:47:29 +0300

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {}
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "public_subnet_cidr" {}
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+variable "azs" {
+  type = list(string)
+}
